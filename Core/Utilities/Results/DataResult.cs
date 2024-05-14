b1c5cb96e5052e6c92f4eaa0a7cb37126d8c6d2f
Aaron4311 <harunkaragoz1@gmail.com>
2024-05-14 16:17:02 +0300

    Results are added

---
+++ Core/Utilities/Results/DataResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Core.Utilities.Results
+{
+	public class DataResult<T> : Result, IDataResult<T>
+	{
+		public DataResult(T data,bool isSucess) : base(isSucess)
+		{
+			Data= data;
+		}
+
+		public DataResult(T data,string message, bool isSucess) : base(message, isSucess)
+		{
+			Data = data;
+		}
+		
+		public T Data { get; }
+	}
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+	public class Result : IResult
+	{
+
+        public Result(bool isSucess)
+        {
+            IsSuccess = isSucess;
+        }
+        public Result(string message, bool isSucess) : this(isSucess) 
+        {
+            Message = message;
+        }
+
+        public string Message { get; }
+
+		public bool IsSuccess { get; }
+	}
+}
