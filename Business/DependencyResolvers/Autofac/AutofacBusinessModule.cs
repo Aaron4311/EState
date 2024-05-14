21d9e1964f30124e6d3835ad00dda331c67fb098
Aaron4311 <harunkaragoz1@gmail.com>
2024-05-14 17:18:14 +0300

    Dependency resolvers are added

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,50 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace Business.DependencyResolvers.Autofac
+{
+	public class AutofacBusinessModule : Module
+    {
+
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<AdvertManager>().As<IAdvertService>().SingleInstance();
+			builder.RegisterType<EfAdvertDal>().As<IAdvertDal>().SingleInstance();
+
+			builder.RegisterType<CityManager>().As<ICityService>().SingleInstance();
+			builder.RegisterType<EfCityDal>().As<ICityDal>().SingleInstance();
+
+			builder.RegisterType<DistrictManager>().As<IDistrictService>().SingleInstance();
+			builder.RegisterType<EfDistrictDal>().As<IDistrictDal>().SingleInstance();
+
+			builder.RegisterType<GeneralSettingsManager>().As<IGeneralSettingsService>().SingleInstance();
+			builder.RegisterType<EfGeneralSettingsDal>().As<IGeneralSettingsDal>().SingleInstance();
+
+			builder.RegisterType<ImageManager>().As<IImageService>().SingleInstance();
+			builder.RegisterType<EfImageDal>().As<IImageDal>().SingleInstance();
+
+			builder.RegisterType<NeighbourhoodManager>().As<INeighbourhoodService>().SingleInstance();
+			builder.RegisterType<EfNeighbourhoodDal>().As<INeighbourhoodDal>().SingleInstance();
+
+			builder.RegisterType<SituationManager>().As<ISituationService>().SingleInstance();
+			builder.RegisterType<EfSituationDal>().As<ISituationDal>().SingleInstance();
+
+			builder.RegisterType<TypeManager>().As<ITypeService>().SingleInstance();
+			builder.RegisterType<EfTypeDal>().As<ITypeDal>().SingleInstance();
+
+			builder.RegisterType<UserAdminManager>().As<IUserAdminService>().SingleInstance();
+			builder.RegisterType<EfUserAdminDal>().As<IUserAdminDal>().SingleInstance();
+			
+
+
+		}
+
+
+
+
+
+	}
+}
